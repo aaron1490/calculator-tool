ae22ec4026d3dcaec3e50cc499579122f3eeb1ce
Aaron Armstrong <aaronarmstrong1490@gmail.com>
2024-01-11 12:10:39 +0000

    Added content to readme.md file providing information on the whole project

--- JS/script.js
+++ JS/script.js
@@ -4,13 +4,13 @@ class Calculator {
     this.currentOperandTextElement = currentOperandTextElement
     this.clear()
     };
-
+// clear function for the calculator
     clear () {
     this.currentOperand = ''
     this.previousOperand = ''
     this.operation = undefined
     }
-
+// DEL aka "backspace" used to remove one character from the back of the array in the calculator
     delete() {
         this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
@@ -35,7 +35,7 @@ class Calculator {
         const prev = parseFloat(this.previousOperand)
         const current = parseFloat(this.currentOperand)
         if(isNaN(prev) || isNaN(current)) return
-        switch (this.operation) {
+        switch (this.operation) { // Switch case to add the working calculator functions
             case '+':
                 computation = prev + current
                 break
@@ -95,6 +95,7 @@ const currentOperandTextElement = document.querySelector('[data-current-operand]
 
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
 
+// event listener for the number buttons including decimal button
 numberButtons.forEach(button => {
     button.addEventListener('click', () =>{
         calculator.appendNumber(button.innerText)
@@ -102,6 +103,7 @@ numberButtons.forEach(button => {
     })
 });
 
+// event listener for the operation buttons (+, -, *, /)
 operationButtons.forEach(button => {
     button.addEventListener('click', () =>{
         calculator.chooseOperation(button.innerText)
@@ -109,16 +111,19 @@ operationButtons.forEach(button => {
     })
 });
 
+// event listener for the "=" button
 equalsButton.addEventListener('click', button => {
     calculator.compute()
     calculator.updateDisplay()
 })
 
+// event listener for the "AC" button
 allClearButton.addEventListener('click', button => {
     calculator.clear()
     calculator.updateDisplay()
 })
 
+// event listener for the "DEL" buttons
 deleteButton.addEventListener('click', button => {
     calculator.delete()
     calculator.updateDisplay()
--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# calculator-tool
-Personal project calculator tool
+# Personal Project: Calculator Tool
+
+## Introduction
+
+Welcome to the respository for a personal project of mine, in this project, I've followed the below Youtube video and have created a Calculator within the web browser.
+
+[Build A Calculator With JavaScript Tutorial](https://www.youtube.com/watch?v=j59qQ7YWLxw)
+
+### The Why?
+
+    The reason for doing this is to supplement my learning on the EdX Front End Dev bootcamp, and to develop upon my current learnings.
+
+## Technologies Used
+
+* HTML5
+* CSS3
+* JavaScript
+
+## Project Structure
+
+The project structure is organized as follows:
+
+* **index.html:** The main HTML file containing the structure of the calculator.
+* **style.css:** The CSS file for styling the calculator.
+* **script.js:** The JavaScript file with the logic for calculator operations.
+
+## Page Demo
+
+![Project Demo](./assets/images/projectDemo.JPG)
+
+## Contact
+
+* [Click here to email me!](mailto:aaronarmstrong1490@gmail.com)
+* [Linkedin](https://www.linkedin.com/in/aaron-armstrong-%E8%89%BE%E4%BF%8A%E6%A8%82-80986ba5/)
+
+## Links
+
+* [Link to the code repository](https://github.com/aaron1490/calculator-tool)
+* [Link to the deployed page]()
+
+## Additional Resources
+
+* [MDN Web Docs - JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+* [W3Schools - HTML Tutorial](https://www.w3schools.com/html/)
+* [CSS-Tricks](https://css-tricks.com/)
+
+## License
+
+MIT License.
+
+## Acknowledgments
+
+I want to take the opportunity to acknowledge the online content that [Web Dev Simplified](https://www.youtube.com/@WebDevSimplified) has put out, they provide plenty of helpful videos relating to the subject of learning front end development.
